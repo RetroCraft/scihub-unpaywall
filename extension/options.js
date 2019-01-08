04b9c65c450966d235947c5e04105f5402a656b6
RetroCraft <retrocraft314@gmail.com>
2019-01-08 16:41:04 -0500

    add sci-hub if doi exists

--- extension/options.js
+++ extension/options.js
@@ -1,37 +1,38 @@
-if (typeof(chrome) !== "undefined"){
-    browser = chrome
+if (typeof chrome !== "undefined") {
+  browser = chrome;
 }
 
 function save_options() {
-    console.log("click")
+  console.log("click");
 
-    var showOaColor = document.getElementById('show-oa-color').checked;
+  var showOaColor = document.getElementById("show-oa-color").checked;
 
-    browser.storage.local.set({
-        showOaColor: showOaColor
-    })
+  browser.storage.local.set({
+    showOaColor: showOaColor
+  });
 
-    var status = document.getElementById('status');
-    status.textContent = 'Preference saved.';
+  var status = document.getElementById("status");
+  status.textContent = "Preference saved.";
 
-    setTimeout(function(){
-        status.textContent = ''
-    }, 1000)
-
-
+  setTimeout(function() {
+    status.textContent = "";
+  }, 1000);
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-    browser.storage.local.get({
-        showOaColor: false
-    }, function(items) {
-        document.getElementById('show-oa-color').checked = items.showOaColor;
-    });
+  browser.storage.local.get(
+    {
+      showOaColor: false
+    },
+    function(items) {
+      document.getElementById("show-oa-color").checked = items.showOaColor;
+    }
+  );
 }
 
-
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('show-oa-color').addEventListener('click',
-    save_options);
+document.addEventListener("DOMContentLoaded", restore_options);
+document
+  .getElementById("show-oa-color")
+  .addEventListener("click", save_options);
--- extension/popup.js
+++ extension/popup.js
@@ -1,24 +1,15 @@
-
-if (chrome){
-    browser = chrome
+if (chrome) {
+  browser = chrome;
 }
 
+document.getElementById("options").addEventListener("click", function() {
+  browser.runtime.openOptionsPage();
+});
 
-document.getElementById("options").addEventListener("click",
-    function(){
-        browser.runtime.openOptionsPage()
-    }
-)
+document.getElementById("homepage").addEventListener("click", function() {
+  browser.tabs.create({ url: "http://unpaywall.org/faq" });
+});
 
-document.getElementById("homepage").addEventListener("click",
-    function(){
-        browser.tabs.create({url: "http://unpaywall.org/faq"})
-    }
-)
-
-document.getElementById("example").addEventListener("click",
-    function(){
-        browser.tabs.create({url: "http://unpaywall.org/welcome"})
-    }
-)
-
+document.getElementById("example").addEventListener("click", function() {
+  browser.tabs.create({ url: "http://unpaywall.org/welcome" });
+});
